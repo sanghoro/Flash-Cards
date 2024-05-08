a145b85677eb28b6c53304a906f0e868cb690e7a
Seong Kang <seong.h.kang2@gmail.com>
2024-05-08 18:49:59 -0500

    completed-iteration1/feat/createCardVariable

--- test/card-test.js
+++ test/card-test.js
@@ -1,19 +1,33 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require('../src/card');
 
-describe('card', function() {
-  it.skip('should be a function', function() {
+describe('card', function () {
+  it('should be a function', function () {
     expect(createCard).to.be.a('function');
   });
 
-  it.skip('should create a card and its properties', function() {
+  it('should create a card and its properties', function () {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    
+
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
     expect(card.correctAnswer).to.equal('object');
-  });  
+  });
 });
+
+describe('guess', function () {
+  it('should return "correct!" when the guess matches the correctAnswer', function () {
+    var correctResult = evaluateGuess('answer', 'answer')
+    expect(correctResult).to.equal('correct!')
+  })
+})
+
+describe('guess', function () {
+  it('should return "incorrect!" when the guess does not match the correctAnswer', function () {
+    var incorrectResult = evaluateGuess('wrongAnswer', 'answer')
+    expect(incorrectResult).to.equal('incorrect!')
+  })
+})
